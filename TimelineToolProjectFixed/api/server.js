a711a00ce1708f210d01340c337d311baab1263e
zwiitors <108046150+zwiitors@users.noreply.github.com>
2024-11-30 11:56:35 +0900

    Update server.js

--- TimelineToolProjectFixed/api/server.js
+++ TimelineToolProjectFixed/api/server.js
@@ -48,10 +48,7 @@ app.post("/api/events/:id/tags", (req, res) => {
 
 const path = require("path");
 
-// フロントエンドのビルドファイルを提供
-app.use(express.static(path.join(__dirname, "build")));
-
-// ルートをフロントエンドにリダイレクト
+app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
